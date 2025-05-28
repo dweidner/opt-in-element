a01c61eeb10c1763474820c3388a3b18ae8830fa
Daniel Weidner <d.weidner@hks-agentur.de>
2025-05-28 15:01:59 +0200

    fix: remove usage of import.meta.url
    The usage of import.meta.url to customize the used tag name might be confusing as it is not supported in most bundlers

--- src/opt-in-element.js
+++ src/opt-in-element.js
@@ -1,6 +1,6 @@
 import {Store} from './store.js';
 
-export class OptIn extends HTMLElement {
+export class OptInElement extends HTMLElement {
   constructor() {
     super();
 
@@ -8,22 +8,30 @@ export class OptIn extends HTMLElement {
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  /** @returns {string[]} */
+  /**
+   * @returns {string[]}
+   */
   get acceptedServices() {
     return this.store.getAll();
   }
 
-  /** @returns {boolean} */
+  /**
+   * @returns {boolean}
+   */
   get accepted() {
     return this.store.contains(this.service);
   }
 
-  /** @returns {string} */
+  /**
+   * @returns {string}
+   */
   get service() {
     return this.getAttribute('service');
   }
 
-  /** @param {?string} value */
+  /**
+   * @param {?string} value
+   */
   set service(value) {
     if (value) {
       this.setAttribute('service', value);
@@ -32,17 +40,23 @@ export class OptIn extends HTMLElement {
     }
   }
 
-  /** @returns {HTMLFormElement} */
+  /**
+   * @returns {?HTMLFormElement}
+   */
   get form() {
     return this.querySelector('form[data-dialog]');
   }
 
-  /** @returns {HTMLInputElement} */
+  /**
+   * @returns {?HTMLInputElement}
+   */
   get checkbox() {
     return this.form?.querySelector('input[name="remember"]');
   }
 
-  /** @returns {HTMLTemplateElement} */
+  /**
+   * @returns {?HTMLTemplateElement}
+   */
   get template() {
     const templateId = this.getAttribute('template') || `${this.localName}-template`;
     const templateElement = document.getElementById(templateId);
@@ -50,7 +64,9 @@ export class OptIn extends HTMLElement {
     return templateElement || this.querySelector('template[data-content]');
   }
 
-  /** @param {?string} value */
+  /**
+   * @param {?string} value
+   */
   set template(value) {
     if (value) {
       this.setAttribute('template', value);
@@ -59,6 +75,9 @@ export class OptIn extends HTMLElement {
     }
   }
 
+  /**
+   * @returns {void}
+   */
   connectedCallback() {
     if (this.accepted) {
       this.showContent();
@@ -67,10 +86,16 @@ export class OptIn extends HTMLElement {
     this.form?.addEventListener('submit', this.handleFormSubmit);
   }
 
+  /**
+   * @returns {void}
+   */
   disconnectedCallback() {
     this.form?.addEventListener('submit', this.handleFormSubmit);
   }
 
+  /**
+   * @returns {this}
+   */
   showContent() {
     this.appendChild(this.template.content.cloneNode(true));
 
@@ -78,9 +103,14 @@ export class OptIn extends HTMLElement {
     this.template.remove();
 
     this.setAttribute('ready', '');
+
+    return this;
   }
 
-  /** @param {SubmitEvent} event */
+  /**
+   * @param {SubmitEvent} event
+   * @returns {void}
+   */
   handleFormSubmit(event) {
     event.preventDefault();
 
@@ -93,34 +123,33 @@ export class OptIn extends HTMLElement {
     this.showContent();
   }
 
-  /** @param {string} tagName */
-  static define(tagName = 'opt-in') {
+  /**
+   * @param {string} tagName
+   * @returns {this}
+   */
+  static define(tagName) {
     if (! ('customElements' in window)) {
       console.warn('Custom elements are not supported by your browser.');
-      return;
+      return this;
     }
 
     const elementName = customElements.getName(this);
 
     if (elementName) {
       console.warn(`${this.name} already defined as <${elementName}>.`);
-      return;
+      return this;
     }
 
     const customElement = customElements.get(tagName);
 
     if (customElement && customElement !== this) {
       console.warn(`<${tagName}> already defined as ${customElement.name}`);
-      return;
+      return this;
     }
 
     customElements.define(tagName, this);
+    return this;
   }
 }
 
-const importUrl = new URL(import.meta.url);
-const preferredTagName = importUrl.searchParams.get('define') || 'opt-in';
-
-if (preferredTagName !== 'false') {
-  OptIn.define(preferredTagName);
-}
+OptInElement.define('opt-in');
